453cf200ed4f025d53e8750eef6f1a79879a7652
Kensho <94515035+DeliveRy1987@users.noreply.github.com>
2024-10-30 23:08:08 +0900

    Delete project3 - beta/bookproject directory

--- project3 - beta/bookproject/book/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from .models import SampleModel
-
-# admin.site.register(SampleModel)
-
-from .models import Book, Review
-
-admin.site.register(Book)
-admin.site.register(Review)
